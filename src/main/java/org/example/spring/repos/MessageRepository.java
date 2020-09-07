a98e731d0a7761aafb408afd05450f4f516e9269
lobin.eugene <43g6ds5rs4>
2020-09-07 22:46:43 +0300

    Initial commit

---
+++ src/main/java/org/example/spring/repos/MessageRepository.java
@@ -0,0 +1,10 @@
+package org.example.spring.repos;
+
+import org.example.spring.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
---
+++ src/main/java/org/example/spring/repos/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.spring.repos;
+
+import org.example.spring.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
